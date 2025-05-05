52dfb5071acf1c66db450b034f2def9bbc57ec91
lckh24 <lecongkhanh242003@gmail.com>
2025-05-05 13:19:00 +0700

    a

--- dags/test.py
+++ dags/test.py
@@ -11,11 +11,15 @@ default_args = {
 def greet(age, ti):
     first_name = ti.xcom_pull(task_ids='get_name', key='first_name')
     last_name = ti.xcom_pull(task_ids='get_name', key='last_name')
+    age = ti.xcom_pull(task_ids='get_age', key='age')
     print(f"Hello World! My name is {first_name} {last_name}, and I am {age} years old!")
 
 def get_name(ti):
     ti.xcom_push(key='first_name', value='Jerry')
     ti.xcom_push(key='last_name', value='Fridman')
+    
+def get_age(ti):
+    ti.xcom_push(keys='age', value=19)
 
 with DAG(
     default_args=default_args,
@@ -32,5 +36,9 @@ with DAG(
         task_id='get_name',
         python_callable=get_name
     )
-    task2 >> task1
+    task3 = PythonOperator(
+        task_id='get_age',
+        python_callable=get_age
+    )
+    [task2,task3] >> task1
     
