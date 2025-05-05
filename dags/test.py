d01d1541e300dbf1074dbb453e4e2ba49780f19a
lckh24 <lecongkhanh242003@gmail.com>
2025-05-05 12:14:01 +0700

    a

--- dags/test.py
+++ dags/test.py
@@ -8,7 +8,8 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
+def greet(age, ti):
+    name = ti.xcoms_pull(task_ids='get_name')
     print(f"Hello World! My name is {name}, and I am {age} years old!")
 
 def get_name():
@@ -20,14 +21,14 @@ with DAG(
     start_date=datetime(2025,5,5),
     schedule_interval='@daily'
 ) as dag:
-    # task1 = PythonOperator(
-    #     task_id='greet',
-    #     python_callable=greet,
-    #     op_kwargs={'name': 'Tom', 'age': 20}
-    # )
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        op_kwargs={'age': 20}
+    )
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name
     )
-    task2
+    task2 >> task1
     
