87d74cd20dc0af8e83406df777d4738465cc1bdb
lckh24 <lecongkhanh242003@gmail.com>
2025-05-05 12:05:41 +0700

    a

--- dags/test.py
+++ dags/test.py
@@ -11,16 +11,23 @@ default_args = {
 def greet(name, age):
     print(f"Hello World! My name is {name}, and I am {age} years old!")
 
+def get_name():
+    return "Jerry"
+
 with DAG(
     default_args=default_args,
     dag_id='first_dag',
     start_date=datetime(2025,5,5),
     schedule_interval='@daily'
 ) as dag:
-    task1 = PythonOperator(
-        task_id='greet',
-        python_callable=greet,
-        op_kwargs={'name': 'Tom', 'age': 20}
+    # task1 = PythonOperator(
+    #     task_id='greet',
+    #     python_callable=greet,
+    #     op_kwargs={'name': 'Tom', 'age': 20}
+    # )
+    task2 = PythonOperator(
+        task_id='get_name',
+        python_callable=get_name
     )
-    task1
+    task2
     
