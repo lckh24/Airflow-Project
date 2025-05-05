1b389e46167cf6b108adbd88eca237515115a6ea
lckh24 <lecongkhanh242003@gmail.com>
2025-05-05 17:26:22 +0700

    update taskflow dag ver2

--- dags/dag_with_taskflow_api.py
+++ dags/dag_with_taskflow_api.py
@@ -14,20 +14,25 @@ default_args = {
 
 def hello_world_etl():
     
-    @task()
+    @task(multiple_outputs=True)
     def get_name():
-        return "Jerry"
+        return {
+            "first_name": "Jerry",
+            "last_name": "Hazel"
+        }
     
     @task()
     def get_age():
         return 19
     
     @task()
-    def greet(name, age):
-        print(f"Hello World! My name is {name} and i am {age} years old")
+    def greet(first_name, last_name, age):
+        print(f"Hello World! My name is {first_name} {last_name} and i am {age} years old")
         
-    name = get_name()
+    name_dict = get_name()
     age = get_age()
-    greet(name=name, age=age)
+    greet(first_name=name_dict['first_name'],
+          last_name=name_dict['last_name'],
+          age=age)
     
 greet_dag = hello_world_etl()
