65c81e41f338afb998738f5ff41b7d1580f51450
lckh24 <lecongkhanh242003@gmail.com>
2025-05-06 12:52:55 +0700

    Test connection

--- dags/test_conn.py
+++ dags/test_conn.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.providers.mysql.hooks.mysql import MySqlHook
 from airflow.hooks.base import BaseHook
 from datetime import datetime
 
@@ -7,13 +8,33 @@ def test_mysql_conn():
     conn = BaseHook.get_connection("mysql")
     print("Host:", conn.host)
     print("Schema:", conn.schema)
+    
+
+def query_mysql():
+    # Tạo kết nối MySQL và thực thi câu truy vấn
+    mysql_hook = MySqlHook(mysql_conn_id="mysql")
+    query = f"""SELECT geolocation_zip_code_prefix, COUNT(*) AS so_lan_xuat_hien
+                FROM geolocation g 
+                GROUP BY geolocation_zip_code_prefix 
+                HAVING COUNT(*) > 1;"""
+    result = mysql_hook.get_records(query)
+    print(result)
+    
+    
 
 with DAG("test_mysql_connection",
          start_date=datetime(2024, 1, 1),
          schedule_interval=None,
          catchup=False) as dag:
-    task = PythonOperator(
+    task1 = PythonOperator(
         task_id="test_conn",
         python_callable=test_mysql_conn
     )
-    task
+    
+    task2 = PythonOperator(
+        task_id="query_mysql",
+        python_callable=query_mysql
+    )
+    task1 >> task2
+    
+    
