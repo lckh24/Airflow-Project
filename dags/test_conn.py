be9ac85a1609ca7ee9a2f2ab674c6d7a766b1b60
lckh24 <lecongkhanh242003@gmail.com>
2025-05-06 12:39:11 +0700

    Test connection

---
+++ dags/test_conn.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.hooks.base import BaseHook
+from datetime import datetime
+
+def test_mysql_conn():
+    conn = BaseHook.get_connection("mysql")
+    print("Host:", conn.host)
+    print("Schema:", conn.schema)
+
+with DAG("test_mysql_connection",
+         start_date=datetime(2024, 1, 1),
+         schedule_interval=None,
+         catchup=False) as dag:
+    task = PythonOperator(
+        task_id="test_conn",
+        python_callable=test_mysql_conn
+    )
