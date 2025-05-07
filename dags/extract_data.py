5487913a18fb0c4f087946156a2b254f2f871d3d
lckh24 <lecongkhanh242003@gmail.com>
2025-05-07 21:57:10 +0700

    a

--- dags/extract_data.py
+++ dags/extract_data.py
@@ -1,9 +1,13 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-from pandas as pd
+import pandas as pd
 from sqlalchemy import create_engine
 
 from plugins.mysql_operator import MySQLOperators
 from plugins.postgresql_operator import PostgresOperators
 
+def extract_and_load_to_staging(**kwargs):
+    source_operator = MySQLOperators('mysql')
+    staging_operator = PostgresOperators('postgres')
+    
