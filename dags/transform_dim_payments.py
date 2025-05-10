66fbeb868311aaf79d51ed995675cfb6bb443b37
lckh24 <lecongkhanh242003@gmail.com>
2025-05-10 18:28:32 +0700

    Update transform dim payments

--- dags/transform_dim_payments.py
+++ dags/transform_dim_payments.py
@@ -13,7 +13,6 @@ def transform_dim_payments():
     
     df['payment_type'] = df['payment_type'].str.lower()
     df['payment_installments'] = df['payment_installments'].fillna(1).astype(int)
-    df['payment_key'] = df.index + 1
     
     # warehouse_operator.save_data_to_postgres(
     #     df,
