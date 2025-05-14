59372cf677dffd990565c0295696bb57bb86d64f
Khanh <lecongkhanh242003@gmail.com>
2025-05-14 08:20:02 +0700

    Ccheck bug

--- dags/transform_dim_order_items.py
+++ dags/transform_dim_order_items.py
@@ -12,16 +12,22 @@ def transform_dim_order_items():
     
     df_order_items = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_order_items")
     df_products = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_products")
-    df_categories = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_product_category_name_translation")    
+    df_categories = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_product_category_name_translation")
+    df_orders = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_orders")[['id', 'order_id']] 
     df = df_order_items.merge(df_products,on='product_id',how='left')
     df.rename(columns={'id_x': 'id',
                    'id_y': 'fk_product_id'}, inplace=True)
     df = df.merge(df_categories, on='product_category_name', how='left')
     df.rename(columns={'id_y': 'fk_product_category_name_id',
-                       'id_x': 'id'}, inplace=True)                   
+                       'id_x': 'id'}, inplace=True)         
+    df = df.merge(df_orders, on='order_id', how='left')
+    df.rename(columns={'id_x': 'id',
+                       'id_y': 'fk_order_id',
+                       'order_id_x': 'order_id'}, inplace=True)          
     df['product_category_name_english'] = df['product_category_name_english'].fillna("Unknown")
     df_columns = ['id',
                   'fk_product_id',
+                  'fk_order_id'
                   'product_category_name_english',
                   'order_item_id',
                   'price',
