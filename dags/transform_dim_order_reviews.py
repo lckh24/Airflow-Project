96063ef03edba7f6899046dcafc673ebef674430
Khanh <lecongkhanh242003@gmail.com>
2025-05-14 10:39:56 +0700

    Ccheck bug

--- dags/transform_dim_order_reviews.py
+++ dags/transform_dim_order_reviews.py
@@ -10,12 +10,16 @@ def transform_dim_order_reviews():
     warehouse_operator = PostgresOperators('postgres')
     
     df = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_order_reviews")
+    df_orders = staging_operator.get_data_to_pd(f"SELECT * FROM staging.stg_orders")
     df['review_creation_date'] = pd.to_datetime(df['review_creation_date'])
     df_latest_reviews = df.sort_values('review_creation_date', ascending=False)\
                           .drop_duplicates(subset='order_id', keep='first')
-    dim_review_key = df_latest_reviews.index + 1
-    dim_review_columns = ['order_id', 'review_score']     
-    
+    df = df.merge(df_orders, on='order_id', how='left')
+    df.rename(columns={'id_x': 'id',
+                       'id_y': 'fk_order_id',
+                       'order_id_x': 'order_id'}, inplace=True) 
+    dim_review_columns = ['id', 'fk_order_id', 'review_score']     
+    df = df[dim_review_columns]
     date = datetime.now()
     execution_date = date.strftime("%d%b%Y")
     df.to_parquet(f'/tmp/dim_reviews_{execution_date}.parquet', index=False)
--- dags/transform_fact_orders.py
+++ dags/transform_fact_orders.py
@@ -6,31 +6,24 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from plugins.postgresql_operator import PostgresOperators
 
 def transform_fact_orders():
-    # Khởi tạo operator kết nối PostgreSQL
     staging_operator = PostgresOperators('postgres')
     warehouse_operator = PostgresOperators('postgres')
     
-    # Lấy dữ liệu từ các bảng staging
     df = staging_operator.get_data_to_pd("SELECT * FROM staging.stg_orders")
     df_customer = staging_operator.get_data_to_pd("SELECT * FROM staging.stg_customers")
     df_order_payments = staging_operator.get_data_to_pd("SELECT * FROM staging.stg_payments")
     
-    # Kết nối df_orders với df_customers dựa trên customer_id
     df = df.merge(df_customer, on='customer_id', how='left')
     
-    # Đổi tên các cột trùng tên (id_x, id_y) sau khi merge
     df.rename(columns={"id_x": "id", "id_y": "fk_customer_id"}, inplace=True)
     
-    # Tính tổng payment_value và số payment_installments lớn nhất cho mỗi order_id
     order_payments_agg = df_order_payments.groupby('order_id').agg({
         'payment_value': 'sum',
         'payment_installments': 'max',
     }).reset_index()
     
-    # Kết hợp dữ liệu thanh toán vào bảng df
     df = df.merge(order_payments_agg, left_on='order_id', right_on='order_id', how='left')
     
-    # Tính toán delivery_time theo số ngày từ khi mua đến khi giao hàng cho khách hàng
     df['order_purchase_timestamp'] = pd.to_datetime(df['order_purchase_timestamp'])
     df['order_delivered_customer_date'] = pd.to_datetime(df['order_delivered_customer_date'])
     
@@ -38,11 +31,9 @@ def transform_fact_orders():
     df['delivery_time'] = (df['order_delivered_customer_date'] - df['order_purchase_timestamp']).dt.total_seconds() / 86400
     df['order_date_key'] = df['order_purchase_timestamp'].dt.date
     
-    # Chọn các cột cần thiết cho bảng fact
     fact_columns = ['id', 'order_id', 'payment_value', 'delivery_time', 'order_date_key', 'fk_customer_id']
     df_fact = df[fact_columns]
-    
-    # Lưu kết quả dưới dạng file parquet
+
     date = datetime.now()
     execution_date = date.strftime("%d%b%Y")
     file_path = f'/tmp/fact_orders_{execution_date}.parquet'
